72d6d34cdc33f6b4dce28a627f129c2de254ee69
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2026-06-15 21:21:34 +1200

    feat: add seo stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,39 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "treysontsen.com",
-  description: "personal website for treyson tsen",
+  title: "Treyson Tsen",
+  description: "this is the personal website of treyson tsen, feel free to click in to read more about me or play some tetris",
   openGraph: {
     type: "website",
     title: "treysontsen.com",
-    description: "personal website for treyson tsen",
+    description: "this is the personal website of treyson tsen, feel free to click in to read more about me or play some tetris",
     url: "https://treysontsen.com",
     images: [{ url: "/website-ogimage.png", width: 1200, height: 630 }],
   },
   twitter: {
     card: "summary_large_image",
     title: "treysontsen.com",
-    description: "personal website for treyson tsen",
+    description: "this is the personal website of treyson tsen, feel free to click in to read more about me or play some tetris",
     images: ["/website-ogimage.png"],
   },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "treysontsen[dot]com",
+              "url": "https://treysontsen.com",
+            }),
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
